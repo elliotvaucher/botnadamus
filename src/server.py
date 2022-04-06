e8e4833061b92a30ea3dfcb44ae118668be21159
Elliot Vaucher <ervaucher@gmail.com>
2022-04-06 13:53:49 +0200

    Revert "update server.py"
    This reverts commit 4db4a64fecad2e65826f258e6c793f7b3bdf8bd1.

--- src/server.py
+++ src/server.py
@@ -1,12 +1,4 @@
 from os import environ
 from flask import Flask
-import botnadamus
-
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    botnadamus.create_api()
-    return
-
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
