4db4a64fecad2e65826f258e6c793f7b3bdf8bd1
Elliot Vaucher <ervaucher@gmail.com>
2022-04-06 13:52:13 +0200

    update server.py

--- src/server.py
+++ src/server.py
@@ -1,4 +1,12 @@
 from os import environ
 from flask import Flask
+import botnadamus
+
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+    botnadamus.create_api()
+    return
+
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
