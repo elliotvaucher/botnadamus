a2922ef88827d8cd18afcb0c7f7986135e226740
Elliot Vaucher <ervaucher@gmail.com>
2022-04-07 10:24:55 +0200

    Update botnadamus.py
    - create a main function to keep the bot working

--- src/botnadamus.py
+++ src/botnadamus.py
@@ -24,19 +24,25 @@ def store_last_seen(FILE_NAME, last_seen_id):
     file_write.close()
     return
 
-tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode="extended")
-
 def get_gif(): 
     response = requests.get("http://api.giphy.com/v1/gifs/random", params=parameters)
     random_gif = json.loads(response.text)
     global gif_url
     gif_url = random_gif['data']['bitly_url']
     
-    return(gif_url)
+    return(gif_url)    
 
-for tweet in reversed(tweets): 
-    if tweet.user.screen_name != "botnadamus":
-        print(str(tweet.id) + ' - ' + tweet.full_text)
-        get_gif()
-        api.update_status('@' + tweet.user.screen_name + ' ' + gif_url, tweet.id)
-        store_last_seen(FILE_NAME, tweet.id)
+def main():
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode="extended")
+    
+    for tweet in reversed(tweets): 
+        if tweet.user.screen_name != "botnadamus":
+            print(str(tweet.id) + ' - ' + tweet.full_text)
+            get_gif()
+            api.update_status('@' + tweet.user.screen_name + ' ' + gif_url, tweet.id)
+            store_last_seen(FILE_NAME, tweet.id)
+    
+
+if __name__ == "__main__":
+    main()
+
