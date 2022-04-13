fc92100565b96c1faa94df707e3aebfac90c2ef7
Elliot Vaucher <ervaucher@gmail.com>
2022-04-13 11:02:42 +0200

    Comments
    - adds some comments to the code

--- src/botnadamus.py
+++ src/botnadamus.py
@@ -7,24 +7,37 @@ from os import environ
 from time import sleep
 import redis
 
+# creating api object from config.py
 api = create_api()
 
+# creating redis object to SET 
+# and GET tweet_id
 r = redis.from_url(os.environ.get("REDIS_URL"))
 
+# passing the GIPHY_KEY value to 
+# access Giphy API
 parameters = {
     'api_key': environ["GIPHY_KEY"]
     }
 
+# setting the initial last tweet_id 
+# at program initialization
 r.mset({"tweet_id": 1512398589099716608})
 
+# function that reads tweet_id from 
+# redis server
 def read_last_seen(): 
     last_seen_id = r.get('tweet_id')
     return last_seen_id
 
+# function that stores the last_seen_id 
+# to redis server
 def store_last_seen(last_seen_id):
     r['tweet_id'] = last_seen_id
     return  
 
+# function that calls the giphy random endpoint
+# and returns a gif_url
 def get_gif(): 
     response = requests.get("http://api.giphy.com/v1/gifs/random", params=parameters)
     random_gif = json.loads(response.text)
@@ -33,6 +46,9 @@ def get_gif():
     
     return(gif_url)    
 
+# main function that calls the Twitter API, 
+# looks for new tweets mentioning the bot, 
+# and prints back a response with gif url.
 def main():
     tweets = api.mentions_timeline(since_id=read_last_seen(), count=1, tweet_mode="extended")
     
